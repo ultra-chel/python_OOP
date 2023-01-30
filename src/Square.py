480bf977324540309f7cde1a3330724a0bc16a05
ultra_chel_qa <frolov@listen2u.ru>
2023-01-30 22:42:49 +0700

    update classes

--- src/Square.py
+++ src/Square.py
@@ -1,21 +1,3 @@
-from src.GeometricFigure import GeometricFigure
-
-#
-# class Square(GeometricFigure):
-#     name = "Square"
-#
-#     def __init__(self, side):
-#         self.side = side
-#
-#     @property
-#     def area(self):
-#         return self.side ** 2
-#
-#     @property
-#     def perimeter(self):
-#         return self.side * 4
-
-
 from src.Rectangle import Rectangle
 from src.Circle import Circle
 
@@ -32,8 +14,8 @@ class Square(Rectangle):
 
 
 square = Square(side=3)
-print(square.area)
+circle = Circle(15)
 
-print(Square.area)
-print(Circle.area)
-print(square.add_area(Circle.area))
+print(square.area)
+print(circle.area)
+print(square.add_area(circle.area))
